36c637744e6e24459a015d456af10d968a8acd47
joshuayannix <joshuayannix@gmail.com>
2020-10-06 09:00:13 -0400

    CRUD

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ function App() {
 
   useEffect(() => {
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
-      console.log(snapshot.docs.map(doc => doc.data().todo))
-    })
+      setTodos(snapshot.docs.map(doc => (
+        { id: doc.id, todo: doc.data().todo }
+      )));
+    });
   }, []);
   
   const addTodo = event => {
@@ -63,7 +64,8 @@ function App() {
       <ul>
         {todos.map(todo => (
           <Todo 
-            text={todo}
+            key={todo.id}
+            todo={todo}
           />
         ))}
       </ul>
--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import React, { useState } from 'react'
+import { List, ListItem, ListItemText, IconButton, Modal, Button } from '@material-ui/core';
 import './Todo.css';
+import db from './firebase';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 function Todo(props) {
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const updateTodo = () => {
+    db.collection('todos').doc(props.todo.id).set({
+      todo: input
+    }, { merge: true })
+    setOpen(false);
+  };
+
   return(
+    <>
+    <Modal
+      open={open}
+      onClose={handleClose}
+    >
+      <form>
+        <h1>MODAL</h1>
+        <input 
+          placeholder={props.todo.todo}
+          value={input}
+          onChange={event => setInput(event.target.value)}
+        />
+        <Button 
+          onClick={updateTodo}
+          color='primary'
+          variant='contained'
+          type='submit'
+        >
+          Update Todo
+        </Button>
+      </form>
+    </Modal>
     <List className='todo__list'>
       <ListItem>
-        <ListItemText primary={props.text} secondary='deadline'/>
+        <ListItemText primary={props.todo.todo} secondary='deadline'/>
       </ListItem>
-      
+      <IconButton
+        color='primary'
+        onClick={e => setOpen(true)}
+      >
+        <EditIcon />
+      </IconButton>
+      <IconButton
+        color='secondary' 
+        onClick={event => db.collection('todos').doc(props.todo.id).delete()}
+      >
+        <DeleteIcon />
+      </IconButton>
     </List>
+    </>
   )
 };
 
