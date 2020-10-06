c2a1a787bd0a395515c50063cfa0930ead4343b5
joshuayannix <joshuayannix@gmail.com>
2020-10-05 20:59:01 -0400

    add todos working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['log hours', 'code', 'bench press']);
+  const [input, setInput] = useState('');
+  console.log('🎃'+ input)
+  
+  const addTodo = event => {
+    event.preventDefault();
+    setTodos([...todos, input])
+    setInput('');
+  }
+
   return (
     <div className="App">
-     <h1>hello world</h1>
+      <h1>hello world</h1>
+      <form action="">
+        <input 
+          value={input} 
+          onChange={event => setInput(event.target.value)}
+        />
+        <button type='submit' onClick={addTodo}>Add todo</button>
+      </form>
+        
+
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
